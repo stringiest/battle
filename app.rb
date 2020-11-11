d34fe54d09eee85aa3e66d65dc5bc755989ad38d
Lucy Stringer <lucyjstringer@gmail.com>
2020-11-11 17:00:21 +0000

    refactor program to move view rendering to get method and use sessions

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Application
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb :play
   end
 
